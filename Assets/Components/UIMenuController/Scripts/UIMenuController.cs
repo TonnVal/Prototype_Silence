bb798c6430f5249c3af27b5566730d8074c2ae31
Erwan Lu <dararith.lu@gmail.com>
2026-03-02 16:01:16 +0100

    Add UIScene and UIMenuController
    [SCENES]
    • Add UIScene.
    • Add UIMenuController which manages game's UI.
    [SCRIPTS]
    • UIMenuController :
         - Open/close menu (+ switch action map Player/UI).
         - Manage exit game according to Unity Editor/build.

---
+++ Assets/Components/UIMenuController/Scripts/UIMenuController.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Components.UIMenuController.Scripts
+{
+    public class UIMenuController : MonoBehaviour
+    {
+        [Header("Menu Inputs")] 
+        [SerializeField] private InputActionAsset _inputActions;
+        
+        [Header("Menu Components")]
+        [SerializeField] private GameObject _menu;
+
+        private InputAction _playerMenuActionRef;
+        private InputAction _uiMenuActionRef;
+
+        private void OnEnable()
+        {
+            _inputActions.FindActionMap("Player").Enable();
+        }
+
+        private void OnDisable()
+        {
+            _inputActions.FindActionMap("Player").Disable();
+        }
+
+        private void Awake()
+        {
+            _playerMenuActionRef = InputSystem.actions.FindAction("Player/Open Menu");
+            _uiMenuActionRef = InputSystem.actions.FindAction("UI/Open Menu");
+        }
+
+        private void Update()
+        {
+            OpenMenu();
+        }
+
+        private void OpenMenu()
+        {
+            if (_playerMenuActionRef.WasPressedThisFrame())
+            {
+                _menu.SetActive(true);
+                _inputActions.FindActionMap("Player").Disable();
+                _inputActions.FindActionMap("UI").Enable();
+            }
+            else if (_uiMenuActionRef.WasPressedThisFrame())
+            {
+                _menu.SetActive(false);
+                _inputActions.FindActionMap("UI").Disable();
+                _inputActions.FindActionMap("Player").Enable();
+            }
+        }
+
+        public void QuitGame()
+        {
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #else
+            Application.Quit();
+            #endif
+        }
+    }
+}
